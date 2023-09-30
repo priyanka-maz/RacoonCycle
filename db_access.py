577d77772ea1848a905bea6f8f11e002d7ba88c3
priyanka-maz <priyankamazumdar0612@gmail.com>
2023-10-01 01:15:49 +0530

    Login Backend (Cookie),  Register Backend (Hashing)

--- db_access.py
+++ db_access.py
@@ -9,3 +9,31 @@ def storePost(dict):
     for i in post_collection.find():
         print(i)
 
+def registerDb(dict):
+    racoon_db = client.racoon_db
+    user_collection = racoon_db.user
+
+    query = {"email": dict['email']}
+    user_document = user_collection.find_one(query)
+    if user_document:
+        #email already exists
+        return None
+    else:
+        user_collection.insert_one(dict)
+        for i in user_collection.find():
+            print(i)
+
+def fetchUserByEmail(email):
+    racoon_db = client.racoon_db
+    user_collection = racoon_db.user
+
+    query = {"email": email}
+
+    # Use find_one to retrieve the first document that matches the filter
+    user_document = user_collection.find_one(query)
+
+    # Check if a user with the given email was found
+    if user_document:
+        return user_document
+    else:
+        return None
